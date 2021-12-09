64424b6deaaa74bcd504b3de0bdbdf8def826800
Timo de Gruiter <timo@EDGITNEW.local>
2021-12-09 11:48:01 +0100

    opdracht afgerond

--- lightswitch.py
+++ lightswitch.py
@@ -1,13 +1,21 @@
+# schijf hier tussen je code
 import tkinter as tk
+from typing import Text
 window = tk.Tk()
 
-button = tk.Button(text='...', bg="white", fg="black")
+button = tk.Button(text= 'Switch light on', bg='white', fg='black')
 button.pack(pady = 20, padx = 20)
 
+def clickButton(event):
+    button.config(text='Switch light on')
+    print ("Switch light on")
+    window['bg'] = 'yellow'
+button.bind("<Button-1>", clickButton)
+
+def clickButton2(event):
+    button.config(text='Switch light off')
+    print ("Switch light off")
+    window['bg'] = 'black'
+button.bind("<Button-3>", clickButton2)
 # schijf hier tussen je code
-
-
-
-# schijf hier tussen je code
-
 window.mainloop()
